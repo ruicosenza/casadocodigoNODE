64b71ffcbb473127915eb076f64b80ac2e6cde92
ruicosenza <ruicosenza@gmail.com>
2017-05-24 16:10:47 -0300

    Configuração da conexão com o BD

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,11 @@
+var connectionFactory = require('../infra/connectionFactory');
 module.exports = function(app){
     app.get('/produtos', function(req,res){
-        res.render("produtos/lista"); 
+        var connection = connectionFactory();
+        
+        connection.query('select * from produtos',function(err,results){
+                res.render('produtos/lista',{lista:results});
+        });
+        connection.end();
     });
 }
